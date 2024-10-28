4d6d5d0509d13fc25be1a3d393f49edfd89cac1c
EsPressOo00 <espresso_o@outlook.com>
2024-10-29 00:02:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const httpProxy = require('http-proxy');
 const port = process.env.PORT || 5000;
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://sgp1.hmvhostings.com:25687/' });
+  proxy.web(req, res, { target: 'http://de01-4.uniplex.xyz:6210/' });
 });
 server.listen(port, () => {
   console.log('Proxy server is running on http://localhost: 5000');
